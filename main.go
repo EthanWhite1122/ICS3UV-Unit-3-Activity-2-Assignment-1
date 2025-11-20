2e80831a1e312f4d494ff8abfe9c0c74fb86473c
Ethan White <ethan.white1@stu.ocsb.ca>
2025-11-20 22:18:04 +0000

    Finished assingment

---
+++ main.go
@@ -0,0 +1,27 @@
+/* Author: Ethan White
+Version: 1.0.0
+Date: 2025-11-19
+This program will round decimals to a certain place
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// Make variables
+	num1 := 8.54667
+	num2 := 9.6382
+	num3 := 18.5146
+	num4 := 125.496
+
+	// Output with rounding
+	fmt.Print(num1)
+	fmt.Print("\n",num2)
+	fmt.Print("\n",num3)
+	fmt.Print("\n",num4)
+	fmt.Println("\nDone.")
+}
+
---
+++ main.ts
@@ -0,0 +1,20 @@
+/**
+ * @author Ethan White
+ * @version 1.0.0
+ * @date 2025-11-19
+ * @fileoverview This program will round decimals to a certain place
+ */
+
+//make variables
+const num1: number = 8.54667
+const num2: number = 9.6382
+const num3: number = 18.5146
+const num4: number = 125.496
+
+//output
+console.log(`${num1.toFixed(3)}`)
+console.log(`${num2.toFixed(5)}`)
+console.log(`${num3.toFixed(1)}`)
+console.log(`${num4.toFixed(1)}`)
+
+console.log("\nDone.")
